6f50d0c7c4d194c94eb428ac35a737a49a06af6c
Neha Gupta <njgupta23@gmail.com>
2018-07-11 18:39:44 -0700

    convert K to F

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,6 +7,12 @@ class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
+        let tempsF = []
+
+        for (let i=0; i<temps.length; i++) {
+            tempsF.push((9/5)*(temps[i]-273)+32);
+        }
+
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         const { lon, lat } = cityData.city.coord;
@@ -14,7 +20,7 @@ class WeatherList extends Component {
         return (
             <tr key={name}>
                 <td><GoogleMap lon={lon} lat={lat} /></td>
-                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={tempsF} color="orange" units="F" /></td>
                 <td><Chart data={pressures} color="green" units="hPa" /></td>
                 <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
@@ -27,7 +33,7 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature (K)</th>
+                        <th>Temperature (F)</th>
                         <th>Pressure (hPa)</th>
                         <th>Humidity (%)</th>
                     </tr>
